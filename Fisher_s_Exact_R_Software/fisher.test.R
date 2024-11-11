cd535c82fbbce02c02866f43be4a6ff78580cc1f
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2024-11-12 06:25:14 +0700

    Use fisher.test With (In) R Software

--- Fisher_s_Exact_R_Software/fisher.test.R
+++ Fisher_s_Exact_R_Software/fisher.test.R
@@ -1,8 +1,8 @@
 # Olah Data Semarang
 # WhatsApp : +6285227746673
 # IG : @olahdatasemarang_
-# Fisher's Exact Test for Count Data Use fisher.test With R Software
-fisher.test = read.csv("https://raw.githubusercontent.com/timbulwidodostp/Fisher_s_Exact_R_Software/fisher.test/fisher.test/fisher.test.csv",sep = ";")
+# Fisher's Exact Test for Count Data Use fisher.test With (In) R Software
+fisher.test = read.csv("https://raw.githubusercontent.com/timbulwidodostp/Fisher_s_Exact_R_Software/main/Fisher_s_Exact_R_Software/fisher.test.csv",sep = ";")
 # Estimation Fisher's Exact Test for Count Data Use fisher.test With R Software
 fisher.test <- fisher.test(fisher.test$Dependen, fisher.test$Independen)
 fisher.test
